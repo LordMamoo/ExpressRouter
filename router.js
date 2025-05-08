16d5c344b0b7e2c57e57522bf13c37efa3fce079
Matthew Tubbs <matthtubbs@gmail.com>
2025-05-08 09:49:38 -0600

    testing router

--- router.js
+++ router.js
@@ -8,17 +8,10 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1>');
+    res.send('<h1>Home There</h1>');
 });
 
 app.get('/students/:id', (req, res) => {
     const id = req.params.id;
     res.send(`This is the Packer path ${id}`);
-});
-
-app.route('/student/search')
-.get((req, res) => {
-    const data = req.query
-    const name = req.query.name
-
 });
